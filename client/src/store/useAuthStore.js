165d6f66956c772d4c088d3bd781b8357ec81f71
AyushPawshe08 <pawsheayush83@gmail.com>
2025-01-17 01:40:14 +0530

    Update useAuthStore.js

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "https://real-time-chatapp-delta.vercel.app" : "/";
+  import.meta.env.MODE === "development" ? "https://social-chain.onrender.com" : "/";
 
 
 export const useAuthStore = create((set, get) => ({
