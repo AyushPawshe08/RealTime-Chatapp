e256225310b798887007ceffff666c906bb6a438
AyushPawshe08 <pawsheayush83@gmail.com>
2025-01-17 01:33:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,12 +27,12 @@ app.get('/', (req, res) => {
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../client/dist")));
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
-    });
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+    })
 }
 
 
