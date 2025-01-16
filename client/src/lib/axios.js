da232b8761a5d989bfcb8c1d0afe21c257160102
AyushPawshe08 <pawsheayush83@gmail.com>
2025-01-16 21:46:53 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE ==="development"? "https://real-time-chatapp-delta.vercel.app": "/api",
+    baseURL: import.meta.env.MODE ==="development"? "https://real-time-chatapp-delta.vercel.app/api": "/api",
     withCredentials:true,
 })
